28312c29070cdeaed63ae7149b6898fc9f9c5d52
Thomas Hoppe <thomas.hoppe@n-fuse.co>
2018-09-29 21:03:43 +0200

    Switch to latest Fontawesome

--- tools/doc-client/web-components/doc-topbar.js
+++ tools/doc-client/web-components/doc-topbar.js
@@ -6,9 +6,9 @@ export default class DocTopbar extends PolymerElement {
   static get template() {
     return html`
     <link rel="stylesheet" href="../vcl.css" media="screen" charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-    <div  class="vclLayoutHorizontal vclLayoutJustified"> 
+    <div  class="vclLayoutHorizontal vclLayoutJustified">
       <h1 class="vclNoMargin">
         <a href="#" title="to index">[[title]]</a>
       </h1>
