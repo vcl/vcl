3bdc77411e8ec0ae9524fc18ae05407008c5d75b
HenriARM <henriarm@yandex.ru>
2019-02-14 15:48:52 +0200

    Updated build-demo to version 0.4.0-3 (fixed error with url resolving)

--- tools/build-demo/index.js
+++ tools/build-demo/index.js
@@ -1,18 +1,20 @@
-
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 const vcl = require('@vcl/preprocessor');
 
 const pack = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 mkdirp('build', (errr) => {
-  if (errr) console.log(errr);
-  const indexHTML = fs.readFileSync(__dirname + '/index.html', 'utf8');
-  const title = pack.name ? 'Demo of: ' + pack.name : 'VCL Demo Page';
-  const finalContent = indexHTML.split('<%- title %>').join(title);
-  fs.writeFileSync('build/index.html', finalContent);
-  vcl.compileFile('./index.sss', 'build/index.css');
+    if (errr) console.log(errr);
+    const indexHTML = fs.readFileSync(__dirname + '/index.html', 'utf8');
+    const title = pack.name ? 'Demo of: ' + pack.name : 'VCL Demo Page';
+    const finalContent = indexHTML.split('<%- title %>').join(title);
+    fs.writeFileSync('build/index.html', finalContent);
+    vcl.compileFile('./index.sss', 'build/index.css', {
+        url: false
+    });
 });
 
 vcl.compileFile('./index.sss', 'build/index.css', {
-  watch: true
+    watch: true,
+    url: false
 });
