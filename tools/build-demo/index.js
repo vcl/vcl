b4bc3547d87b9ca315fe2963c5e71df25657f955
Daniel Hartmann <contact@dk2.de>
2020-01-16 12:43:55 +0100

    wip

--- tools/build-demo/index.js
+++ tools/build-demo/index.js
@@ -3,19 +3,27 @@
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 const path = require('path');
-const childProcess = require('child_process');
+const sass = require('sass');
+const yargs = require('yargs')
+              .describe('vcl-root', 'root directory for vcl imports');
 
+var argv = yargs.argv;
 const PCK = path.resolve(process.cwd(), './package.json');
-const INDEX_SRC = path.resolve(__dirname, './index.html');
-const INDEX_TRG = path.resolve(process.cwd(), './build/index.html');
-
-const PREPROCESSOR_SCRIPT_PATH = path.resolve(process.cwd(), 'node_modules/@vcl/preprocessor/bin/vcl.js');
-
 const pack = JSON.parse(fs.readFileSync(PCK, 'utf8'));
 
+const sassFile = pack.scssDemo || pack.scss;
+
+if (!sassFile) {
+  throw new Error('scssDemo or scss not defined in package.json');
+}
+
 // Use pkg demoStyle as primary source for demo but fallback to style if it does not exist
-const SSS_SRC = path.resolve(process.cwd(), pack.demoStyle || pack.style);
-const SSS_TRG = path.resolve(process.cwd(), './build/index.css');
+const SASS_SRC = path.resolve(process.cwd(), sassFile);
+const SASS_TRG = path.resolve(process.cwd(), './build/index.css');
+const SASS_MAP_TRG = path.resolve(process.cwd(), './build/index.css.map');
+
+const INDEX_SRC = path.resolve(__dirname, './index.html');
+const INDEX_TRG = path.resolve(process.cwd(), './build/index.html');
 
 mkdirp.sync('build');
 const indexHTML = fs.readFileSync(INDEX_SRC, 'utf8');
@@ -23,22 +31,33 @@ const title = pack.name ? 'Demo of: ' + pack.name : 'VCL Demo Page';
 const finalContent = indexHTML.replace('<%- title %>',title);
 fs.writeFileSync(INDEX_TRG, finalContent);
 console.log('Created ' + INDEX_TRG);
+console.log('Building ' + SASS_SRC);
 
-console.log('Building ' + SSS_SRC);
+const includePaths = [path.resolve(process.cwd(), 'node_modules')];
 
-const proc = childProcess.fork(PREPROCESSOR_SCRIPT_PATH, [
-  SSS_SRC,
-  SSS_TRG,
-  ...process.argv.slice(2)
-]);
+const vclRoot = argv['vcl-root'];
 
-// listen for errors as they may prevent the exit event from firing
-proc.on('error', function (err) {
-  console.error(err);
-  process.exit();
-});
-
-// execute the callback once the process has finished running
-proc.on('exit', function (code) {
-  process.exit(code);
+sass.render({
+  file: SASS_SRC,
+  sourceMap: true,
+  outFile: SASS_TRG,
+  includePaths,
+  importer: function(url, prev, done) {
+    if (vclRoot && url.startsWith('@vcl/')) {
+      const file = path.resolve(process.cwd(), vclRoot, url.substr(5), 'index.scss');
+      return {
+        file
+      };
+    }
+    return undefined;
+  }
+}, function(error, result) {
+  if(!error){
+    fs.writeFileSync(SASS_TRG, result.css);
+    console.log('Created ' + SASS_TRG);
+    fs.writeFileSync(SASS_MAP_TRG, result.map);
+    console.log('Created ' + SASS_MAP_TRG);
+  } else {
+    console.error(error);
+  }
 });
