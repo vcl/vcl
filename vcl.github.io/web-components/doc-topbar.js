8f6866af37abaaacdb3eb777816652b9fc251fb0
Daniel Hartmann <contact@dk2.de>
2020-01-17 12:51:27 +0100

    wip

--- vcl.github.io/web-components/doc-topbar.js
+++ vcl.github.io/web-components/doc-topbar.js
@@ -8,13 +8,13 @@ export default class DocTopbar extends PolymerElement {
     <link rel="stylesheet" href="../styles.css" media="screen" charset="utf-8">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-    <div  class="vclHor vclJustifyBetween">
-      <h1 class="vclM-0">
+    <div  class="row justify-between">
+      <h1 class="m-0">
         <a href="#" title="to index">[[title]]</a>
       </h1>
       <div>
         <a href="https://github.com/vcl/vcl" target="_blank" title="The VCL on Github">
-          <span class="vclIcon fab fa-github fa-3x"></span>
+          <span class="icon fab fa-github fa-3x"></span>
         </a>
       </div>
     </div>`;
