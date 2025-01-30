841b742c7fdeb77b94c13c927e97c27cfce48f4c
Bello Babakolo <fleetbeekay@gmail.com>
2025-01-30 01:54:10 +0100

    chore: remove references to font awesome

--- docgen/vcl.github.io/web-components/doc-demo.js
+++ docgen/vcl.github.io/web-components/doc-demo.js
@@ -5,7 +5,6 @@ export default class DocDemo extends PolymerElement {
   static get template() {
     return html`
     <link rel="stylesheet" href="../styles.css" media="screen" charset="utf-8">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
     <div id="docDemo">
       <div class="tabbable">
         <div class="tabs tab-style-uni" role="tablist">
